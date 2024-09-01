12c57558c422e19a0906ea701f3286fa07c38574
NithinKonda <nithinkonda142@gmail.com>
2024-09-01 16:57:16 +0530

    Fix paths

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,46 +1,48 @@
 import React from "react";
+import GroupGPTLogo from '../assets/groupgpt.jpg'
+import portfolioLogo from '../assets/portfolio.png'
+import leaderboardLogo from '../assets/leaderboard.png'
+import hfestLogo from '../assets/hfest23.png'
+import goodolLogo from '../assets/good-ol.png'
+import chessLogo from '../assets/chessy.jpg'
+import gitHubLogo from '../assets/github.svg'
+import gitIvertLogo from '../assets/gitinvert.png'
 
 const projects = [
   {
     title: "GroupGPT",
-    description:
-      "A AI chat-app where you can collaborate with your team with a GPT included.",
-    imageUrl: "src/assets/groupgpt.jpg",
+    description: "An AI chat-app where you can collaborate with your team with a GPT included.",
+    imageUrl: GroupGPTLogo,
     githubLink: "https://github.com/saiteja1290/grpgpt",
   },
   {
     title: "THIS WEBSITE",
-    description:
-      "The portfolio website which is made for monospace and minimilastic designing concept.",
-    imageUrl: "src/assets/portfolio.png",
+    description: "The portfolio website which is made for monospace and minimalistic designing concept.",
+    imageUrl: portfolioLogo,
     githubLink: "https://github.com/NithinKonda/portfolio",
   },
   {
     title: "CP Leaderboard",
-    description:
-      "Website for tracking coding progress and rankings across platforms like LeetCode. Join the community, compete, and climb the ranks in competitive coding.",
-    imageUrl: "src/assets/leaderboard.png",
+    description: "Website for tracking coding progress and rankings across platforms like LeetCode. Join the community, compete, and climb the ranks in competitive coding.",
+    imageUrl: leaderboardLogo,
     githubLink: "https://github.com/cbitosc/LeaderBoard",
   },
   {
     title: "COSC Hacktoberfest 2023",
-    description:
-      "The central hub for CBIT Hacktoberfest Hackathon 2023, offering dynamic responsiveness, captivating animations, and seamless event registration.",
-    imageUrl: "src/assets/hfest23.png",
+    description: "The central hub for CBIT Hacktoberfest Hackathon 2023, offering dynamic responsiveness, captivating animations, and seamless event registration.",
+    imageUrl: hfestLogo,
     githubLink: "https://github.com/cbitosc/cbit-hacktoberfest23",
   },
   {
     title: "Good-Ol",
-    description:
-      "A basic arrow game that is compatible with any desktop can be played in any browser.",
-    imageUrl: "src/assets/good-ol.png",
+    description: "A basic arrow game that is compatible with any desktop can be played in any browser.",
+    imageUrl: goodolLogo,
     githubLink: "https://github.com/NithinKonda/good-ol/",
   },
   {
     title: "Chesscom",
-    description:
-      "A chess.com clone which is implemented with Websocktes for real time game play.",
-    imageUrl: "src/assets/chessy.jpg",
+    description: "A chess.com clone which is implemented with Websockets for real-time gameplay.",
+    imageUrl: chessLogo,
     githubLink: "https://github.com/NithinKonda/chesscom",
   },
 ];
@@ -69,12 +71,12 @@ const Projects = () => {
             >
               {/* GitHub icon that changes on hover */}
               <img
-                src="src/assets/github.svg"
+                src={gitHubLogo}
                 alt="GitHub Link"
                 className="w-6 h-6 mr-2 hover:hidden"
               />
               <img
-                src="src/assets/gitinvert.png"
+                src={gitIvertLogo}
                 alt="GitHub Link"
                 className="w-6 h-6 hidden hover:inline"
               />
